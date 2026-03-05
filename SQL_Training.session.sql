3e62a996a35d4e881dc378eca987d504d1b61ac9
sasasan03 <sako.hiro62@gmail.com>
2026-03-05 17:25:27 +0900

    3月5日作業内容

--- SQL_Training.session.sql
+++ SQL_Training.session.sql
@@ -1,4 +1,4 @@
------------------------------------------------- 🟥テーブル作成
+-- テーブル作成
 CREATE TABLE Shops (
     shop_id INT AUTO_INCREMENT PRIMARY KEY,
     shop_name VARCHAR(20),
@@ -19,12 +19,12 @@ CREATE TABLE Address (
     sex VARCHAR(3),
     age INT
 );
------------------------------------------------- 🟥テーブルカラム修正
+-- テーブルカラム修正
 ALTER TABLE Address
 MODIFY phone_number VARCHAR(30),
     MODIFY age INT NOT NULL;
 SHOW TABLES;
------------------------------------------------- 🟥レコード追加
+-- レコード追加
 INSERT INTO Shops (shop_name, rating, area) VALUE ('サンプルショップ', 1, '和歌山');
 INSERT INTO Address (name, phone_number, address, sex, age) VALUE ('鈴木', '080-1111-2222', '東京', '男', 24),
     ('鈴木', '080-1111-2222', '東京', '男', 24),
@@ -37,18 +37,18 @@ INSERT INTO Address (name, phone_number, address, sex, age) VALUE ('鈴木', '08
     ('中村', '090-8888-9999', '神戸', '女', 26),
     ('小林', NULL, '広島', '男', 33),
     ('加藤', '080-1234-5678', '仙台', '女', 30);
------------------------------------------------- 🟥テーブルの中身検索
+-- テーブルの中身検索
 SELECT *
 FROM Shops;
 SELECT *
 FROM Reservations;
 SELECT *
 FROM Address;
------------------------------------------------- 🟥JOIN
+-- JOIN
 SELECT shop_name
 FROM Shops S
     INNER JOIN Reservations R ON S.shop_id = R.shop_id;
------------------------------------------------- 🟥絞り込み
+-- 絞り込み
 SELECT name,
     address
 FROM Address
@@ -76,7 +76,7 @@ SELECT address,
     count(*)
 FROM Address
 GROUP BY address;
------------------------------------------------- 🟥特定レコード修正
+-- 特定レコード修正
 -- UPDATE Address SET address = '石川' WHERE name = '田中' OR name = '小林' OR name = '中村';
 UPDATE Address
 SET address = '東京'
@@ -341,3 +341,383 @@ VALUES ('2024-06-01', 'Sat', 120),
     ('2024-06-18', 'Tue', 72),
     ('2024-06-19', 'Wed', 85),
     ('2024-06-20', 'Thu', 92);
+------------------------------------------------- 3/5（木）
+-- 社員が兼任している結果セットを返す
+-- CASE式を使って結果セットを返す
+SELECT emp_name,
+    CASE
+        WHEN COUNT(*) = 1 THEN MAX(team) -- group byを使っているため集約関数に包む必要がある
+        WHEN COUNT(*) = 2 THEN '２つ兼務'
+        WHEN COUNT(*) >= 3 THEN '３つ以上を兼務'
+        ELSE NULL
+    END AS team
+FROM Employees
+GROUP BY emp_name;
+-- UNIONを使って結果セットを返す
+SELECT emp_name,
+    MAX(team) AS team
+FROM Employees
+GROUP BY emp_name
+HAVING COUNT(*) = 1
+UNION
+SELECT emp_name,
+    '２つを兼務' AS team
+FROM Employees
+GROUP BY emp_name
+HAVING COUNT(*) = 2
+UNION
+SELECT emp_name,
+    '３つ以上兼務' AS team
+FROM Employees
+GROUP BY emp_name
+HAVING COUNT(*) >= 3;
+-- UNIONを使用しなければならないケース
+-- 条件①：UNIONを使ってうまくインデックスでレコードの絞り込みができる場合。（ペアにINDEXを指定する）
+-- 条件②：UNION以外の手段ではインデックス検索をうまく使用できない場合。
+-- テーブル作成
+CREATE TABLE ThreeElements (
+    id INT,
+    -- 『key』はエラーが発生
+    name VARCHAR(20),
+    date_1 DATE,
+    flg_1 VARCHAR(1),
+    date_2 DATE,
+    flg_2 VARCHAR(1),
+    date_3 DATE,
+    flg_3 VARCHAR(1)
+);
+-- レコード挿入
+INSERT INTO ThreeElements(
+        id,
+        name,
+        date_1,
+        flg_1,
+        date_2,
+        flg_2,
+        date_3,
+        flg_3
+    ) VALUE (1, 'a', '2026-3-1', 'T', NULL, NULL, NULL, NULL),
+    (2, 'b', NULL, NULL, '2026-3-1', 'T', NULL, NULL),
+    (3, 'c', NULL, NULL, '2026-3-1', 'F', NULL, NULL),
+    (4, 'd', NULL, NULL, '2026-3-12', 'T', NULL, NULL),
+    (5, 'e', NULL, NULL, NULL, NULL, '2026-3-1', 'T'),
+    (6, 'f', NULL, NULL, NULL, NULL, '2026-3-14', 'F');
+-- UNIONを使ってテーブルの結果セットを返す
+EXPLAIN
+SELECT id,
+    name,
+    date_1,
+    flg_1,
+    date_2,
+    flg_2,
+    date_3,
+    flg_3
+FROM ThreeElements
+WHERE date_1 = '2026-3-1'
+    AND flg_1 = 'T'
+UNION
+SELECT id,
+    name,
+    date_1,
+    flg_1,
+    date_2,
+    flg_2,
+    date_3,
+    flg_3
+FROM ThreeElements
+WHERE date_2 = '2026-3-1'
+    AND flg_2 = 'T'
+UNION
+SELECT id,
+    name,
+    date_1,
+    flg_1,
+    date_2,
+    flg_2,
+    date_3,
+    flg_3
+FROM ThreeElements
+WHERE date_3 = '2026-3-1'
+    AND flg_3 = 'T';
+-- data_nとflg_nをペアにし、INDEXを指定することで高速で検索することが可能
+CREATE INDEX IDX_1 ON ThreeElements (date_1, flg_1);
+CREATE INDEX IDX_2 ON ThreeElements (date_2, flg_2);
+CREATE INDEX IDX_3 ON ThreeElements (date_3, flg_3);
+-- 作成したINDEXを確認することが可能
+SHOW INDEX
+FROM ThreeElements;
+-- INDEXを削除
+DROP INDEX IDX_1 ON ThreeElements;
+DROP INDEX IDX_2 ON ThreeElements;
+DROP INDEX IDX_3 ON ThreeElements;
+-- ORを使っても同様の結果セットを返すことができる
+EXPLAIN
+SELECT id,
+    name,
+    date_1,
+    flg_1,
+    date_2,
+    flg_2,
+    date_3,
+    flg_3
+FROM ThreeElements
+WHERE (
+        date_1 = '2026-3-1'
+        AND flg_1 = 'T'
+    )
+    OR (
+        date_2 = '2026-3-1'
+        AND flg_2 = 'T'
+    )
+    OR (
+        date_3 = '2026-3-1'
+        AND flg_3 = 'T'
+    );
+-- INを使っても同様の値を返す
+SELECT id,
+    name,
+    date_1,
+    flg_1,
+    date_2,
+    flg_2,
+    date_3,
+    flg_3
+FROM ThreeElements
+WHERE (CAST('2026-3-1' AS DATE), 'T') -- ペアにする場合は2026-3-1ではヒットしない
+    IN (
+        (date_1, flg_1),
+        (date_2, flg_2),
+        (date_3, flg_3)
+    );
+CREATE TABLE NonAggTbl (
+    id VARCHAR(32) NOT NULL,
+    data_type CHAR(1) NOT NULL,
+    data_1 INT,
+    data_2 INT,
+    data_3 INT,
+    data_4 INT,
+    data_5 INT,
+    data_6 INT
+);
+INSERT INTO NonAggTbl
+VALUES ('jim', 'a', 10, 20, 30, 40, 50, 60),
+    ('jim', 'b', 15, 25, 35, 45, 55, 65),
+    ('jim', 'c', 5, 15, 25, 35, 45, 55),
+    ('ken', 'a', 100, 200, 300, 400, 500, 600),
+    ('ken', 'b', 110, 210, NULL, 410, 510, 610),
+    ('ken', 'c', 120, 220, 320, 420, 520, 620),
+    ('beth', 'a', 7, 17, 27, NULL, 47, 57),
+    ('beth', 'b', 8, 18, 28, 38, NULL, 58),
+    ('beth', 'c', 9, NULL, 29, 39, 49, 59);
+-- date_typeがAの時はdata_1,2
+-- date_typeがBの時はdata_3,4,5
+-- date_typeがCの時はdata_6
+-- nameでまとめる。使用するdataのみをidでまとめた結果セットを返す
+-- 文字や日付であればMAX()かMIN()がふさわしい。（結果はAVGやSUMでも結果は変わらない）
+EXPLAIN
+SELECT id,
+    MAX(
+        CASE
+            WHEN data_type = 'a' THEN data_1
+            ELSE NULL
+        END
+    ) AS data_1,
+    MAX(
+        CASE
+            WHEN data_type = 'a' THEN data_2
+            ELSE NULL
+        END
+    ) AS data_2,
+    MAX(
+        CASE
+            WHEN data_type = 'b' THEN data_3
+            ELSE NULL
+        END
+    ) AS data_3,
+    MAX(
+        CASE
+            WHEN data_type = 'b' THEN data_4
+            ELSE NULL
+        END
+    ) AS data_4,
+    MAX(
+        CASE
+            WHEN data_type = 'b' THEN data_5
+            ELSE NULL
+        END
+    ) AS data_5,
+    MAX(
+        CASE
+            WHEN data_type = 'c' THEN data_6
+            ELSE NULL
+        END
+    ) AS data_6
+FROM NonAggTbl
+GROUP by id;
+-- 集約操作
+-- テーブル作成
+CREATE TABLE PriceByAge (
+    product_id VARCHAR(32) NOT NULL,
+    low_age INT NOT NULL,
+    high_age INT NOT NULL,
+    price INT NOT NULL,
+    PRIMARY KEY (product_id, low_age),
+    CHECK(low_age < high_age)
+);
+-- レコード挿入
+INSERT INTO PriceByAge VALUE ('製品１', 0, 50, 2000),
+    ('製品１', 51, 100, 3000),
+    ('製品２', 0, 50, 2000),
+    ('製品３', 0, 20, 500),
+    ('製品３', 31, 70, 800),
+    ('製品３', 71, 100, 1000),
+    ('製品４', 0, 99, 4000);
+-- 上記レコードの中から0~100歳までの人全員が遊べる製品を探し出す
+SELECT product_id
+FROM PriceByAge
+GROUP BY product_id
+HAVING SUM(high_age - low_age + 1) = 101;
+-- 上記と同様の問題
+CREATE TABLE HotelRooms (
+    room_nbr INT,
+    start_date DATE,
+    end_date DATE,
+    PRIMARY KEY(room_nbr, start_date)
+);
+INSERT INTO HotelRooms VALUE (101, '2024-02-01', '2024-02-06'),
+    (101, '2024-02-06', '2024-02-08'),
+    (101, '2024-02-10', '2024-02-13'),
+    (201, '2024-02-05', '2024-02-08'),
+    (201, '2024-02-08', '2024-02-11'),
+    (201, '2024-02-11', '2024-02-12'),
+    (301, '2024-02-03', '2024-02-17');
+SELECT room_nbr
+FROM HotelRooms
+GROUP BY room_nbr
+HAVING SUM(end_date - start_date) >= 10;
+SELECT room_nbr,
+    SUM(end_date - start_date) as worknig_days
+FROM HotelRooms
+GROUP BY room_nbr
+HAVING SUM(end_date - start_date) > 10;
+CREATE TABLE Persons (
+    name VARCHAR(8) NOT NULL,
+    age INT NOT NULL,
+    height FLOAT NOT NULL,
+    weight FLOAT NOT NULL,
+    PRIMARY KEY (name)
+);
+INSERT INTO Persons VALUE ('鈴木', 29, 180, 70),
+    ('佐藤', 35, 175, 80),
+    ('高橋', 28, 182, 75),
+    ('伊藤', 40, 170, 68),
+    ('渡辺', 33, 178, 72),
+    ('山本', 26, 185, 85),
+    ('中村', 31, 176, 73),
+    ('小林', 38, 181, 77),
+    ('加藤', 27, 174, 69);
+DROP TABLE Persons;
+SELECT SUBSTRING(name, 1, 1) as label,
+    COUNT(*)
+FROM Persons
+GROUP BY SUBSTRING(name, 1, 1);
+SELECT CASE
+        WHEN age < 20 THEN '20未満'
+        WHEN age BETWEEN 20 AND 30 THEN '青年'
+        WHEN age >= 30 THEN 'おじさん'
+        ELSE NULL
+    END AS age_class,
+    COUNT(*)
+FROM Persons
+GROUP BY CASE
+        WHEN age < 20 THEN '20未満'
+        WHEN age BETWEEN 20 AND 30 THEN '青年'
+        WHEN age >= 30 THEN 'おじさん'
+        ELSE NULL
+    END;
+-- 記載できるが標準違反
+SELECT CASE
+        WHEN age < 20 THEN '20未満'
+        WHEN age BETWEEN 20 AND 30 THEN '青年'
+        WHEN age >= 30 THEN 'おじさん'
+        ELSE NULL
+    END AS age_class,
+    COUNT(*)
+FROM Persons
+GROUP BY age_class;
+-- これでも出力可
+SELECT name,
+    weight / ((height * 0.01) * (height * 0.01)) AS BMI,
+    CASE
+        WHEN weight / ((height * 0.01) * (height * 0.01)) >= 25 THEN '肥満'
+        WHEN weight / ((height * 0.01) * (height * 0.01)) BETWEEN 18.5 AND 25 THEN '普通'
+        WHEN weight / ((height * 0.01) * (height * 0.01)) < 20 THEN 'やせ'
+        ELSE NULL
+    END AS classification
+FROM Persons;
+-- POWERを使った方法
+SELECT CASE
+        WHEN weight / POWER(height / 100, 2) < 18.5 THEN 'やせ'
+        WHEN 18.5 <= weight / POWER(height / 100, 2) < 25 THEN '標準'
+        WHEN weight / POWER(height / 100, 2) >= 25 THEN '肥満'
+        ELSE NULL
+    END AS bmi,
+    COUNT(*)
+FROM Persons
+GROUP BY bmi;
+-- PARTITION BY句
+SELECT name,
+    CASE
+        WHEN age < 20 THEN '子供'
+        WHEN age BETWEEN 20 AND 30 THEN '青年'
+        WHEN age > 30 THEN 'おじさん'
+        ELSE NULL
+    END AS age_class,
+    RANK() OVER(
+        PARTITION BY CASE
+            WHEN age < 20 THEN '子供'
+            WHEN age BETWEEN 20 AND 30 THEN '青年'
+            WHEN age > 30 THEN 'おじさん'
+            ELSE NULL
+        END
+        ORDER BY age -- パーティションで分けられた要素をageで並び替える
+    ) AS age_rank_in_class
+FROM Persons
+ORDER BY age_class,
+    age_rank_in_class;
+CREATE TABLE Sales (company VARCHAR(20), year INT, sale INT);
+INSERT INTO Sales VALUE ('A', 2002, 50),
+    ('A', 2003, 52),
+    ('A', 2004, 55),
+    ('A', 2005, 55),
+    ('B', 2001, 27),
+    ('B', 2005, 28),
+    ('B', 2001, 28),
+    ('B', 2001, 30),
+    ('C', 2001, 40),
+    ('C', 2005, 39),
+    ('C', 2006, 38),
+    ('C', 2010, 35);
+CREATE TABLE Sales2 (
+    company VARCHAR(20),
+    year INT,
+    sale INT,
+    var VARCHAR(5)
+);
+INSERT INTO Sales2
+SELECT company,
+    `year`,
+    sale,
+    CASE
+        SIGN(
+            sale - MAX(sale) OVER (
+                PARTITION BY company
+                ORDER BY `year` ROWS BETWEEN 1 PRECEDING AND 1 PRECEDING
+            )
+        )
+        WHEN 0 THEN '='
+        WHEN 1 THEN '+'
+        WHEN -1 THEN '-'
+        ELSE NULL
+    END AS var
+FROM Sales;
